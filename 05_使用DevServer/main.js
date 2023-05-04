89bdfd55ef28463a686bdeb2025fbb8b9e44bc73
vnyoon <vnyoon@163.com>
2023-05-04 14:27:47 +0800

    feat: 使用DevServer开启服务本地文件预览、实时预览、模块热替换、source map

---
+++ 05_使用DevServer/main.js
@@ -0,0 +1,11 @@
+// 通过 CommonJS 规范导入 CSS 模块
+require('./main.css');
+// require('style-loader!css-loader!./main.css');
+
+// 通过 CommonJS 规范导入 show 函数
+const showContent = require('./show.js');
+
+// 执行 show 函数
+showContent('Webpack');
+
+console.log('main.js - 实时预览log~');
